960947fecb7ab9ab6de65c05ce7fec4f3b891df5
Benjamin Simmers <bej1380@gmail.com>
2023-07-12 19:45:51 +1000

    Setup Package

--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function base64ToArrayBuffer(base64: string): ArrayBuffer;
-export declare function handleDownload(data: any): void;
-export declare function deconstructFile(filename: any): any;
-export declare function FileTypeSchema(): string;
