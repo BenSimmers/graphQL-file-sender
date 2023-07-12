92d136473204bbfd7918a6bc252e7ff868634800
Benjamin Simmers <bej1380@gmail.com>
2023-07-12 23:36:47 +1000

    basic pdf generation template

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Option } from 'funfix';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import * as mimeTypes from 'mime-types';
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 
 export const base64ToArrayBuffer = (base64: string) =>
   Uint8Array.from(window.atob(base64), (char) => char.charCodeAt(0)).buffer;
@@ -18,17 +19,58 @@ export function handleDownload(data: any) {
     download: filename,
   });
 }
+const PdfGenerationParamsTests = {
+  fontSize: 24,
+  textContent: 'Hello, World!',
+  x: 0,
+  y: 0,
+};
 
-export function deconstructFile(filename: any, baseDirectory: string) {
-  return Option.of(filename)
-    .map((f) => {
-      const file = join(baseDirectory, f);
-      const fileContent = readFileSync(file);
-      const base64Content = Buffer.from(fileContent).toString('base64');
-      const fileMime = mimeTypes.lookup(file) ?? '';
-      const fileObj = { filename, mimetype: fileMime, encoding: 'base64', content: base64Content };
+interface PdfGenerationParams {
+  fontSize: number;
+  textContent: string;
+  x: number;
+  y: number;
+}
 
-      return fileObj;
+const defaultPdfGenerationFunction = async (params: PdfGenerationParams) => {
+  const { fontSize, textContent, x, y } = params;
+
+  const pdfDoc = await PDFDocument.create();
+  const page = pdfDoc.addPage();
+  const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+  const textWidth = font.widthOfTextAtSize(textContent, fontSize);
+  const textHeight = font.heightAtSize(fontSize);
+  const xPos = (page.getWidth() - textWidth) / 2 + x;
+  const yPos = (page.getHeight() - textHeight) / 2 + y;
+
+  page.drawText(textContent, {
+    x: xPos,
+    y: yPos,
+    size: fontSize,
+    font,
+    color: rgb(0, 0, 0),
+  });
+
+  const pdfBytes = await pdfDoc.save();
+  const base64String = Buffer.from(pdfBytes).toString('base64');
+  const fileMime = mimeTypes.lookup('pdf') ?? '';
+
+  // return the file object
+  return {
+    filename: 'test.pdf',
+    mimetype: fileMime,
+    encoding: 'base64',
+    content: base64String,
+  };
+};
+
+export function deconstructFile(baseDirectory: any, generatePdfFn = defaultPdfGenerationFunction) {
+  return Option.of(baseDirectory)
+    .map((_) => {
+      const fileContent = generatePdfFn(PdfGenerationParamsTests);
+
+      return fileContent;
     })
     .getOrElse({
       filename: '',
